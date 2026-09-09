8a3c952b8555c135cc031e0bbf46551810696574
thuhangkhuat <thuhangkt01@gmai.com>
2026-09-10 00:43:09 +0700

    modify scenario

--- CreateModel.m
+++ CreateModel.m
@@ -10,29 +10,21 @@ function model=CreateModel()
     [X,Y] = meshgrid(x,y); % replicate x & y to create a rectangular grid (X,Y) 
 
     % Generate Probability map
-    mu1 = [10 10];  % Mean of first distribution (Potential target position)
+    mu1 = [2*MAP_SIZE/4 MAP_SIZE/2];  % Mean of first distribution (Potential target position)
     Sigma1 = MAP_SIZE*[0.1 0;0 0.1]; % Covarian of the distribution
     F1 = mvnpdf([X(:) Y(:)],mu1,Sigma1);
     F1 = reshape(F1,length(y),length(x));  % Convert F to matrix
     F1 = F1/sum(F1(:)); % Scale to total 1
     
-    mu2 = [5 5];
+    mu2 = [3*MAP_SIZE/4 MAP_SIZE/2];
     Sigma2 = MAP_SIZE*[0.1 0;0 0.1];
     F2 = mvnpdf([X(:) Y(:)],mu2,Sigma2);
     F2 = reshape(F2,length(y),length(x));  % Convert F to matrix
     F2 = F2/sum(F2(:));
-    Pmap = 0.5*(F1 + F2); % Standardise the map with two target info sources
 
-    %pcolor(X,Y,Pmap);
-    %{
-    %Plot probabilistic map
-    figure();
-    surf(x,y,Pmap);
-    caxis([min(Pmap(:))-.5*range(Pmap(:)),max(Pmap(:))]); % Set colour range
-    axis([0 MAP_SIZE 0 MAP_SIZE 0 max(Pmap(:))]);
-    xlabel('x'); ylabel('y'); zlabel('Probability Density');
-    %}
-   
+    Pmap = F1+F2; % Standardise the map with two target info sources
+    Pmap = Pmap/sum(Pmap(:));
+ 
     
     % Map limits
     xmin= -floor(MAP_SIZE/2);
@@ -42,8 +34,8 @@ function model=CreateModel()
     ymax= floor(MAP_SIZE/2);
     
     % Initial searching position
-    xs=0;
-    ys=0;
+    xs=-5;
+    ys=-5;
     
     % Number of path nodes (not including the start position (start node))
     n=20;
@@ -65,5 +57,5 @@ function model=CreateModel()
     model.X = X;
     model.Y = Y;
     model.targetMoves = 10; % Must be divisible by the path Length (e.g, mod(N,move)=0)
-    model.targetDir = 'E';
+    model.targetDir = 'NW';
 end
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This repository presents the following article in MATLAB:
 
-Thu Hang Khuat, Hai Trung Le, Manh Duong Phung,  "**Minimum Time Search of a Lost Target Using Multimodal Particle Swarm Optimization with Multiple Unmanned Aerial Vehicles**", PrePrint, 2026
+Thu Hang Khuat, Hai Trung Le, Khanh Thanh Tran, Manh Duong Phung,  "**Minimum Time Search of a Lost Target Using Multimodal Particle Swarm Optimization with Multiple Unmanned Aerial Vehicles**", PrePrint, 2026
 
 ## Installation
 ```
@@ -10,22 +10,22 @@ git@github.com:thuhangkhuat/EMPSO_target_search.git
 ```
 
 ## Run simulation
-The current version have six different scenarios. Before run each method, please change the value of `model` in file. To run our method:
+To run our method:
 1. Download all the source files from this repository.
 2. Open MATLAB.
 3. Execute the main script by running `EMPSO.m`.
 
 ## Visualization
 
-Below are visualizations of the method for Scenario 1:
+Below are visualizations of the method:
 
-| ![Scenario 1](scen1_init.png) | ![Search path ](scen1_path.png) |
+| ![Scenario](scen.png) | ![Search path ](path.png) |
 |:---:|:---:|
 
 ## Citation
 ```
 @ARTICLE{EMPSO2026,
-  author={Thu Hang Khuat, Hai Trung Le, Manh Duong Phung},
+  author={Thu Hang Khuat, Hai Trung Le, Khanh Thanh Tran and Manh Duong Phung},
   journal={PrePrint}, 
   title={Minimum Time Search of a Lost Target Using Multimodal Particle Swarm Optimization with Multiple Unmanned Aerial Vehicles}, 
   year={2026},
